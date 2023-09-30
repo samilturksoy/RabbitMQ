7dd44cc4a8a95376d8ab7dc698b0e0db6ca6c645
Samil Turksoy <Samil.Turksoy@unilever.com>
2023-09-30 15:45:35 +0300

    basit que message birakildi

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ();
+
+//baglanti olustur
+factory.Uri = new Uri("amqps://fwxftggn:KeOuBKBQW1kC7aXO0mefLOQOkjTI4pk0@sparrow.rmq.cloudamqp.com/fwxftggn");
+
+// Baglanti aktiflestirme ve kanal acma
+using IConnection connection = factory.CreateConnection ();
+using IModel channel = connection.CreateModel();
+
+//queve olusturma
+channel.QueueDeclare(queue: "example-queve", exclusive: false);
+
+//queve mesaj gonderme RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message);
+
+Console.Read();
