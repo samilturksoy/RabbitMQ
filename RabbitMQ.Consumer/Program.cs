2f5919859fc2e87d24abc0ab7daf1338bc5870f9
Samil Turksoy <Samil.Turksoy@unilever.com>
2023-09-30 16:36:11 +0300

    consume message on docker

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Bağlantı Oluşturma
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+//factory.Uri = new Uri("..");
+factory.HostName = "localhost";
+// Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queve oluşturma 
+channel.QueueDeclare(queue: "example-queve", exclusive: false);
+
+//Queve mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queve", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //kuyruğa gelen mesajın işlediğim yer
+    //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir
+    //e.Body.Span veya e.Body.ToArray() : Kuyruktaki mesajın byte verisini getirecektir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,11 +1,12 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-ConnectionFactory factory = new ();
+
 
 //baglanti olustur
-factory.Uri = new Uri("amqps://fwxftggn:KeOuBKBQW1kC7aXO0mefLOQOkjTI4pk0@sparrow.rmq.cloudamqp.com/fwxftggn");
-
+ConnectionFactory factory = new();
+//factory.Uri = new Uri("..."); cloud url kullanılacaksa
+factory.HostName = "localhost"; //docker üzerinden 
 // Baglanti aktiflestirme ve kanal acma
 using IConnection connection = factory.CreateConnection ();
 using IModel channel = connection.CreateModel();
@@ -15,7 +16,14 @@ channel.QueueDeclare(queue: "example-queve", exclusive: false);
 
 //queve mesaj gonderme RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.
 
-byte[] message = Encoding.UTF8.GetBytes("Merhaba");
-channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message);
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message);
+}
 
 Console.Read();
