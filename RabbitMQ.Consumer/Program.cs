97fb0feb992df415a249bcecf3e9a0c05e66eb6c
Samil Turksoy <Samil.Turksoy@unilever.com>
2023-09-30 18:18:51 +0300

    consumer aldıgı mesajı basarili olarak isledigini publishera bildirdi ve publishler da rmq cokerse messajlar kalıcı hale getirildi

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -15,13 +15,15 @@ channel.QueueDeclare(queue: "example-queve", exclusive: false);
 
 //Queve mesaj okuma
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "example-queve", false, consumer);
+channel.BasicConsume(queue: "example-queve", autoAck: false, consumer);
 consumer.Received += (sender, e) =>
 {
     //kuyruğa gelen mesajın işlediğim yer
     //e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir
     //e.Body.Span veya e.Body.ToArray() : Kuyruktaki mesajın byte verisini getirecektir.
     Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false); //Kuyruktaki mesajı okuduğunu işlediğini publishera bildirmek için kullanılır ki kuyruktan message silinsin
 };
 
 Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -12,6 +12,7 @@ using IConnection connection = factory.CreateConnection ();
 using IModel channel = connection.CreateModel();
 
 //queve olusturma
+// RabitMQ sunucusu çökerse kuruktaki tüm messagelar kayboluyor bunu önleme için öncelikle 1.yapılandırma QueueDeclare dyrable:true değerini veriyoruz
 channel.QueueDeclare(queue: "example-queve", exclusive: false);
 
 //queve mesaj gonderme RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir.
@@ -19,11 +20,14 @@ channel.QueueDeclare(queue: "example-queve", exclusive: false);
 //byte[] message = Encoding.UTF8.GetBytes("Merhaba");
 //channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message);
 
+//RabitMQ sunucusu çökerse kuruktaki tüm messagelar kayboluyor bunu önleme için 2.yapılandırma properties oluşturuluyor.
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
     byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message);
+    channel.BasicPublish(exchange: "", routingKey: "example-queve", body: message,basicProperties:properties);
 }
 
 Console.Read();
