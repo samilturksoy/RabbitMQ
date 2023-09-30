cde228a25ef2a2d96d3abdc5fd4b0fc3d22725a6
Samil Turksoy <Samil.Turksoy@unilever.com>
2023-09-30 22:11:56 +0300

    direct-exchange ornegi

---
+++ RabbitMQ.Direct.Exchange.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//1.Adım
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+//2.Adım
+string queveName = channel.QueueDeclare().QueueName;
+
+//3.Adım
+channel.QueueBind(queue: queveName,
+                 exchange: "direct-exchange-example",
+                 routingKey : "direct-queve-example");
+
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(consumer: consumer,queue : queveName,autoAck : true);
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+Console.Read();
+
+
+//1. Adım : Publisherdaki 1e1 aynı isiml ve type'a sahip bir exchange tanımlanmalıdır !
+//2.Adım : Publisher tarafından router keyde bulunan değerdeki kuyruğa gönderilen mesajları kendi oluşturduğumuz
+//kuyruğa yönlendirerek tüketmemiz gerekmektedir Bunun için öncelikle bir kuyruk oluşturulmalıdır.
---
+++ RabbitMQ.Direct.Exchange.Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.HostName = "localhost";
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "direct-exchange-example", 
+                        routingKey: "direct-queve-example",
+                        body: byteMessage);
+}
+
+Console.Read();
